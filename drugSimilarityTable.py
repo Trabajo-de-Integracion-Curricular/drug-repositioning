a3797d50723aaa625677f08c0c678416ae486df3
Juan Pablo Rodriguez <rodriguezjp4@gmail.com>
2022-01-26 12:44:49 -0500

    Update/1.1 Implementation of the Similarity Table
    Refactor of the SimilarityTableClass and Processing the first molecule with radius 2 & 4

--- drugSimilarityTable.py
+++ drugSimilarityTable.py
@@ -1,21 +1,34 @@
 class SimilarityTable:
-    drugsSimilarities = {}
+    drugs_similarities = {}
+    less_value = 0.0
+    num_elements = 10
 
     def __init__(self, drug_id):
         self.drug_id = drug_id
 
-    def addItem(self, item_id, item_value):
-        if len(self.drugsSimilarities) < 10:
-            self.drugsSimilarities.update({item_id: item_value})
-            self.orderDictionarity()
+    def add_item(self, item_id, item_value):
+        if len(self.drugs_similarities) < self.num_elements or item_value > self.less_value:
+            self.drugs_similarities.update({item_id: item_value})
+            self.order_dictionary()
 
-    def orderDictionarity(self):
-        sortedSimilarities = sorted(self.drugsSimilarities.items(), key=lambda x:x[1], reverse=True)
-        newDrugsSimilarities = {}
-        for value in sortedSimilarities:
-            newDrugsSimilarities[value[0]] = value[1]
-        self.drugsSimilarities = newDrugsSimilarities
+    def order_dictionary(self):
+        sorted_similarities = sorted(self.drugs_similarities.items(), key=lambda x: x[1], reverse=True)
+        new_drugs_similarities = {}
+        dictionary_len = len(sorted_similarities)
 
-    def printDictionarity(self):
-        for key, value in self.drugsSimilarities:
+        if dictionary_len < self.num_elements:
+            for i in range(dictionary_len):
+                new_drugs_similarities[sorted_similarities[i][0]] = sorted_similarities[i][1]
+                if i == dictionary_len - 1:
+                    self.less_value = sorted_similarities[i][1]
+        else:
+            for i in range(self.num_elements):
+                new_drugs_similarities[sorted_similarities[i][0]] = sorted_similarities[i][1]
+                if i == self.num_elements - 1:
+                    self.less_value = sorted_similarities[i][1]
+
+        self.drugs_similarities = new_drugs_similarities
+
+    def print_dictionary(self):
+        for key, value in self.drugs_similarities.items():
             print(key, value)
